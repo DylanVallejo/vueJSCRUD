73ac4c1f75634a2e7f94c17a32bda4ec350218e8
DylanVallejo <dylan1811vallejo@gmail.com>
2023-03-07 22:13:12 -0500

    usando props y emits con composition api

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="container">
         <div class="post">
+            <!-- <h5>{{ props.title }}</h5>
+            <p>{{ props.content }}</p> -->
+            <!-- compositon api -->
             <h5>{{ props.title }}</h5>
             <p>{{ props.content }}</p>
-            <input  type="text" v-model="message"/>
-            <button @click="handleClick" >Alert Father Component</button>
+            <input class="input"  type="text" v-model="message"/>
+            <button @click="handleClick" >Alert Father </button>
         </div>
     </div>
 </template>
-<script>
+<!-- <script >
 import { defineComponent,ref } from 'vue';
 export default defineComponent({
     name:"PostDetail",
@@ -35,11 +38,38 @@ export default defineComponent({
         
         return {props, message,handleClick}
     }
+    
+    // composition api no se usa setup sino data
+    // data(props, {emit}){
+    //     let message = ref("")
+    //     const handleClick = () => { 
+    //         console.log(message)
+    //         emit("alertFather", message.value)
+    //     }
+        // debemos usar this en las props y emits 
+    //     return {this.props, message,handleClick}
+    // }
+    
 })
+</script> -->
+
+<!-- usando composition api para propr y emits -->
+<script setup>
+    // importamos defineporps y emits para definirlos
+    import { ref, defineProps, defineEmits } from 'vue';
+
+    const props = defineProps({title :String, content: String});
+    const emit = defineEmits(['alertFather']);
+    let message = ref("")
+    const handleClick = () => { 
+        console.log(message)
+        emit("alertFather", message.value)
+    }
+
 </script>
 <style scoped>
 .container .post{
-    width: 200px;
+    width: auto;
     height:  150px;
     border-radius: 10px;
     background-color: bisque;
@@ -49,5 +79,14 @@ export default defineComponent({
 }
 .container h4{
     text-align: center;
+}
+.container .input{
+    width: 100px;
+    display: block;
+}
+.container button{
+    max-width: auto;
+    margin: 2px 0;
+    display: block;
 }
 </style>
